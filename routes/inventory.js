9f9bff57ba572131641d4c1f4e72c2a9b7ccd919
Chris Thierer <cthierer1@gmail.com>
2014-11-23 13:34:10 -0500

    Refactor inventory routes

--- routes/inventory.js
+++ routes/inventory.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var path = require('path');
 
 /* GET home page. */
-router.get('/products', function(req, res) {
+router.get('/', function(req, res) {
   var context = req.app.get('appContext');
   var dataService = context.get('dataService');
 
   dataService.getProducts(function (err, products) {
-    res.render('inventory/products/index.hbs', { 
+    res.render('inventory/index', { 
       title: 'Current Inventory',
       products: products 
     });
