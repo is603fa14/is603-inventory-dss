468bcd29d0985b30819a453674fa079b1ccde186
Chris Thierer <cthierer1@gmail.com>
2014-11-23 13:47:25 -0500

    Add XML dependencies and clean-up formatting

--- services/XmlService.js
+++ services/XmlService.js
@@ -4,53 +4,49 @@ var _ = require('underscore');  // http://underscorejs.org/#
 var parseString = require('xml2js').parseString;
 var Product = require('../models/Product');
 var WeeklySale = require('../models/WeeklySale');
+
 var XmlService = function (fileName) {
   // filename must be a string, and must point to a real file
   assert(_.isString(fileName), "File name must be a path to an XML file");
   this.fileName = fileName;
 
-    this.getProducts = function (callback) {
+  this.getProducts = function (callback) {
     var fileContents = fs.readFileSync(this.fileName);
     var products = [];
-    // TODO parse contents into product array
-    // TODO define product class
-    // (both of these depends on the spreadsheet format)
-    parseString(fileContents,function(err,result)
-	{
-                
-                var xml_products=result.products.product;
-                
-                for(var key in xml_products)
-                {
-                    var xmlproduct = xml_products[key];
-                    var product = new Product({
-                        type: xmlproduct['$']['type'],
-                        name: xmlproduct['$']['name'],
-                        retailPrice: xmlproduct['retailPrice'],
-                        margin: xmlproduct['margin']
-                    });
-                
-                    for (var weekKey in xmlproduct['week']) {
-                        var xmlweek = xmlproduct['week'][weekKey];
-                        var week = new WeeklySale({
-                            id: xmlweek['$']['id'],
-                            quantity: xmlweek['qty'],
-                            marketAverage: xmlweek['mkavg'],
-                            numStores: xmlweek['nos'],
-                            promo: xmlweek['$']['promo']
-                        });
-                
-                        product.addWeeklySale(week);
-                    }
-                    products.push(product);
-                
-                }
-                
-                callback(products);
-    	
-	})
 
-  };
+    parseString(fileContents,function(err,result) {
+        var xml_products=result.products.product;
+        
+        for(var key in xml_products)
+        {
+            var xmlproduct = xml_products[key];
+            var product = new Product({
+                type: xmlproduct['$']['type'],
+                name: xmlproduct['$']['name'],
+                retailPrice: xmlproduct['retailPrice'],
+                margin: xmlproduct['margin']
+            });
+        
+            for (var weekKey in xmlproduct['week']) {
+                var xmlweek = xmlproduct['week'][weekKey];
+                var week = new WeeklySale({
+                    id: xmlweek['$']['id'],
+                    quantity: xmlweek['qty'],
+                    marketAverage: xmlweek['mkavg'],
+                    numStores: xmlweek['nos'],
+                    promo: xmlweek['$']['promo']
+                });
+        
+                product.addWeeklySale(week);
+            }
+            products.push(product);
+        
+        }
+        
+        callback(products);
+
+	});
+  }
 };
 
 module.exports = XmlService;
